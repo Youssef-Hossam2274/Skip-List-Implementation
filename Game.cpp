40eaf00ccfcb3833997633f20de6addc7a46e3ed
Eslam-Sayed7 <eslam.sayed.ed@gmail.com>
2024-12-12 18:22:04 +0200

    FIX:output operator

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#inwclude "SkcipList.cpp"
+#include "SkipList.cpp"
 using namespace std;
 
 
@@ -30,24 +30,117 @@ public:
     bool operator>=(const Player& other) const {
         return this->score >= other.score;
     }
-
-    void operator<<(const Player& other) const {
-        cout << this->Name << "   " << this->score << endl;
-    }
+   friend ostream& operator<<(ostream& os, const Player& other) {
+        os << other.Name << "   " << other.score;
+        return os;
+    } 
 
 };
 
-class GameManger {
-public:
-    
-GameManger(){}
-    void Join(string name , int score = 0){
-        
-    }
-    void Leave(string name){
+// class GameManger {
+// public:
+//     SkipList<Player> board;
+//     GameManger(){
 
-    }
-    void GetTop_N_Players(){
+//     }
+//     void Join(string name , int score = 0){
+//         Player player = Player(name, score);
+//         board.insert(player);
+//     }
+//     void Leave(string name){
+//         board.Delete(name);
+//     }
+//     void GetTop_N_Players(int N){
         
-    }
-};
+//         board.printTopN(N);
+//     }
+// };
+
+
+// void printMenu() {
+//     cout << "Game Menu:" << endl;
+//     cout << "1. Join Game" << endl;
+//     cout << "2. Print Player Score" << endl;
+//     cout << "3. Print Top N Players" << endl;
+//     cout << "4. Leave Game" << endl;
+//     cout << "5. Exit" << endl;
+//     cout << "Enter your choice: ";
+// }
+
+// void menu() {
+//     int choice;
+//     while (true) {
+//         printMenu();
+//         cin >> choice;
+
+//         switch (choice) {
+//             case 1: {
+//                 joinGameMenu();
+//                 break;
+//             }
+//             case 2: {
+//                 printPlayerScoreMenu();
+//                 break;
+//             }
+//             case 3: {
+//                 printTopNPlayersMenu();
+//                 break;
+//             }
+//             case 4: {
+//                 leaveGameMenu();
+//                 break;
+//             }
+//             case 5:
+//                 return;
+//             default:
+//                 cout << "Invalid choice. Please try again." << endl;
+//         }
+//     }
+// }
+
+// void joinGameMenu() {
+//     string name;
+//     int score;
+//     cout << "Enter your name: ";
+//     cin >> name;
+//     cout << "Enter your initial score: ";
+//     cin >> score;
+//     addPlayer(Player(name, score));
+//     cout << "You have joined the game!" << endl;
+// }
+
+// void printPlayerScoreMenu() {
+//     string name;
+//     cout << "Enter the name of the player: ";
+//     cin >> name;
+//     for (const auto& player : players) {
+//         if (player.Name == name) {
+//             cout << "Player " << name << " score: " << player.score << endl;
+//             return;
+//         }
+//     }
+//     cout << "Player not found." << endl;
+// }
+
+// void printTopNPlayersMenu() {
+//     int N;
+//     cout << "Enter the number of top players to display: ";
+//     cin >> N;
+//     printLeaderboard(N);
+// }
+
+// void leaveGameMenu() {
+//     string name;
+//     cout << "Enter your name: ";
+//     cin >> name;
+//     removePlayer(name);
+//     cout << "You have left the game." << endl;
+// }
+
+
+int main() {
+    // menu();
+    Player p = Player("Eslam" , 5);
+    cout << p; 
+    return 0;
+}
