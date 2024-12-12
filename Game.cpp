61adda856a9b5d8096e8fcfd1184a27d046755e3
Eslam-Sayed7 <eslam.sayed.ed@gmail.com>
2024-12-12 17:37:25 +0200

    ADD: classes & operators overloading

---
+++ Game.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#inwclude "SkcipList.cpp"
+using namespace std;
+
+
+class Player {
+public:
+    string Name;
+    int score;
+
+    Player(string name, int score) {
+        this->Name = name;
+        this->score = score;
+    }
+
+
+    bool operator<(const Player& other) const {
+        return this->score < other.score;
+    }
+
+    bool operator>(const Player& other) const {
+        return this->score > other.score;
+    }
+    bool operator==(const Player& other) const {
+        return this->score == other.score;
+    }
+    bool operator<=(const Player& other) const {
+        return this->score <= other.score;
+    }
+    bool operator>=(const Player& other) const {
+        return this->score >= other.score;
+    }
+
+    void operator<<(const Player& other) const {
+        cout << this->Name << "   " << this->score << endl;
+    }
+
+};
+
+class GameManger {
+public:
+    
+GameManger(){}
+    void Join(string name , int score = 0){
+        
+    }
+    void Leave(string name){
+
+    }
+    void GetTop_N_Players(){
+        
+    }
+};
